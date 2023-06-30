8dd2a28bcbf884466f6e17ce6763fa6202ab518f
KARTIK5799 <thekartiksapkal@gmail.com>
2023-06-30 20:06:27 +0530

    datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=12;
+    cout<<a<<endl;
+
+    char b = 'a';
+    cout<<b<<endl;
+
+    bool c =true;
+    cout<<c<<endl;
+
+    float d= 1.56;
+    cout<<d<<endl;
+
+    double e= 1.25;
+    cout<<e<<endl;
+
+    int size=sizeof(a);
+    cout<<"size of a is :- "<<size<<endl;
+   int z;
+    
+    return 0;
+}
