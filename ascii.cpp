14c471f940a51ed642dceedab27f9664ccc64a51
KARTIK5799 <thekartiksapkal@gmail.com>
2023-06-30 20:12:08 +0530

    ascii

---
+++ ascii.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    char a;
+    cout<<"enter the value :";
+    cin>>a;
+    cout<<"Ascii value of your given "<<a<<" is "<<int(a);
+    return 0;
+}
